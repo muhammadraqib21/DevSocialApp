67529e44d88cc26ce4db5cea0685ba5d460746b4
Muhammad Raqib <muhammad.raqib1@gmail.com>
2019-10-07 15:25:28 -0400

    Added routes and MongoDB config

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require ('express');
-
+const connectDb = require('./config/db');
 const app = express ();
 
-app.get('/', (req,res)=> res.send('API RUNNING'));
+//connect DB 
+connectDb();
+
+app.get('/', (req,res) => res.send('API RUNNING'));
+
+// define routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/profile', require('./routes/api/profile'));
 
 const PORT = process.env.PORT || 5000;
 
